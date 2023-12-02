5e184bb764f3a622db759e1a3f29f386b8b42689
kadendavis866 <kadendavis866@u.boisestate.edu>
2023-12-02 16:53:55 -0700

    rest api implementation

---
+++ databaseAPI/src/main/java/rest/models/Route.java
@@ -0,0 +1,52 @@
+package rest.models;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "user_posts")
+public class UserPost {
+
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField
+    private String username;
+
+    @DatabaseField
+    private int routeID;
+
+    @DatabaseField
+    private String comment;
+
+    public UserPost() {
+        // ORMLite needs a no-arg constructor
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getRouteID() {
+        return routeID;
+    }
+
+    public void setRouteID(int routeID) {
+        this.routeID = routeID;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
