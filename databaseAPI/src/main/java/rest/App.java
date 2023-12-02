eb860a54aedb28eab9f6eded075ab35285c269cc
kadendavis866 <kadendavis866@u.boisestate.edu>
2023-12-02 02:22:37 -0700

    set up server take 2

--- databaseAPI/src/main/java/rest/App.java
+++ databaseAPI/src/main/java/rest/App.java
@@ -1,18 +1,9 @@
 package rest;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
-
-public class Main {
+public class App {
     public static void main(String[] args) {
-        get(new Route("/users/:id") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return  "User: username=test, email=test@test.net";
-            }
-        });
+        get("/hello", (req, res) -> "Hello World");
     }
 }
