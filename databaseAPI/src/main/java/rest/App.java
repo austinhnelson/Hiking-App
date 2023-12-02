d913a9c28f300d532649f40d2f5b8ad4b59a3146
kadendavis866 <kadendavis866@u.boisestate.edu>
2023-12-02 01:38:54 -0700

    set up server

--- databaseAPI/src/main/java/rest/App.java
+++ databaseAPI/src/main/java/rest/App.java
@@ -1,13 +1,18 @@
 package rest;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import static spark.Spark.get;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class Main {
+    public static void main(String[] args) {
+        get(new Route("/users/:id") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return  "User: username=test, email=test@test.net";
+            }
+        });
     }
 }
