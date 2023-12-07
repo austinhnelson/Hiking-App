f5bbecca963beffc5ca75c318e0e12468fa9d4f6
kadendavis866 <kadendavis866@u.boisestate.edu>
2023-12-06 21:50:11 -0700

    menu bars added

--- App.js
+++ App.js
@@ -1,29 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import {Button, PermissionsAndroid, Platform, StyleSheet, Text, View} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, SafeAreaView, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 import * as RemoteAccess from './components/RemoteAccess';
+import {Buttons, Colors} from './styles/index'
 
 export default function App() {
-  const clicked = () => {
-      //RemoteAccess.loadPosts().then(value => console.log(value));
-      //RemoteAccess.loadPost(1).then(value => console.log(value));
-      //RemoteAccess.loadPostsByUser('my name').then(value => console.log(value));
-      RemoteAccess.loadRoute(1).then(value => alert(value.points));
-      //RemoteAccess.loadRoutesByUser('my name').then(value => console.log(value));
-  };
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title='click me' onPress={clicked}/>
-      <StatusBar style="auto" />
-    </View>
-  );
+    /*const clicked = () => {
+        //RemoteAccess.loadPosts().then(value => console.log(value));
+        //RemoteAccess.loadPost(1).then(value => console.log(value));
+        //RemoteAccess.loadPostsByUser('my name').then(value => console.log(value));
+        RemoteAccess.loadRoute(1).then(value => alert(value.points));
+        //RemoteAccess.loadRoutesByUser('my name').then(value => console.log(value));
+    };*/
+
+    const doNothing = () => {};
+
+    const MyButton = (text, onPress) => {
+        return (
+            <TouchableHighlight style={styles.button} onPress={onPress}>
+                <Text style={styles.text}>{text}</Text>
+            </TouchableHighlight>
+        );
+    };
+
+    const TopBar = (
+        <View style={styles.buttonBar}>
+            {MyButton('T1', doNothing)}
+            {MyButton('T2', doNothing)}
+        </View>
+    );
+
+    const BottomBar = (
+        <View style={styles.buttonBar}>
+            {MyButton('B1', doNothing)}
+            {MyButton('B2', doNothing)}
+        </View>
+    );
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar style="auto"/>
+            {TopBar}
+            <View style={styles.screen}></View>
+            {BottomBar}
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        flex: 1,
+        ...Buttons.smallRounded,
+    },
+    text: {
+        color: Colors.black,
+    },
+    buttonBar: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: Colors.menuBar,
+    },
+    screen:{
+        flex: 10,
+        backgroundColor: Colors.background,
+    },
 });
