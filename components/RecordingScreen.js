8ccce9157a14f2c7953930ad49804971ea7e10ff
kadendavis866 <kadendavis866@u.boisestate.edu>
2023-12-07 17:26:54 -0700

    geolocation working

--- components/RecordingScreen.js
+++ components/RecordingScreen.js
@@ -1,11 +1,33 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {Colors} from '../styles/index';
-import MapView from "react-native-maps";
+import MapView, {Marker} from "react-native-maps";
+import * as Location from 'expo-location'
+import {useEffect, useState} from "react";
 
 export const RecordScreen = () => {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+
     return (
         <View style={styles.view}>
-            <MapView style={styles.map}></MapView>
+            <MapView style={styles.map}>
+                <Marker coordinate={location !== null ? location.coords : {latitude: 0, longitude: 0}}/>
+            </MapView>
         </View>
     );
 }
