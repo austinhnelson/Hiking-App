13ae1969e01e02a3a940f7a6a519ac259aee9f37
kadendavis866 <kadendavis866@u.boisestate.edu>
2023-12-07 23:45:21 -0700

    distance calculation

--- components/RecordingScreen.js
+++ components/RecordingScreen.js
@@ -2,8 +2,31 @@ import {Image, StyleSheet, View} from 'react-native';
 import {Colors} from '../styles/index';
 import MapView, {Marker, Polyline} from "react-native-maps";
 
+function calculateDistance(coords) {
+    let distance = 0;
+    //// Haversine formula ////
+    // Radius of earth in miles
+    const r = 3959;
+    for (let i = 0; i < coords.length - 1; i++) {
+        let c1 = coords[i];
+        let c2 = coords[i + 1];
+        // convert latitudes to radians
+        const lat1 = c1.latitude * Math.PI / 180;
+        const lat2 = c2.latitude * Math.PI / 180;
+        // magic math formula
+        const dlat = lat2 - lat1;
+        const a = Math.pow(Math.sin(dlat / 2), 2)
+            + Math.cos(lat1) * Math.cos(lat2)
+            * Math.pow(Math.sin((c2.longitude - c1.longitude) * Math.PI / 360),2);
+        distance += 2 * r * Math.asin(Math.sqrt(a));
+    }
+    return distance;
+}
+
 export const RecordScreen = ({locations, mapRef}) => {
     let latestCoords;
+    let distance = 0;
+
     if (locations.length > 0 && mapRef.current) {
         latestCoords = locations[locations.length - 1].coords;
         mapRef.current.animateToRegion({
@@ -12,6 +35,10 @@ export const RecordScreen = ({locations, mapRef}) => {
             latitudeDelta: 0.005,
             longitudeDelta: 0.005,
         }, 1500);
+        if(locations.length > 1){
+            distance = calculateDistance(locations.map(l=>(l.coords)));
+            console.log('distance: ' + distance.toPrecision(3) + 'mi');
+        }
     }
     return (
         <View style={styles.view}>
