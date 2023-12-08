73e9053827a78c23e3761eaa435ab183f4b1e39f
kadendavis866 <kadendavis866@u.boisestate.edu>
2023-12-07 18:56:16 -0700

    location update are recording

--- components/RecordingScreen.js
+++ components/RecordingScreen.js
@@ -2,31 +2,56 @@ import {StyleSheet, View} from 'react-native';
 import {Colors} from '../styles/index';
 import MapView, {Marker} from "react-native-maps";
 import * as Location from 'expo-location'
+import * as TaskManager from 'expo-task-manager';
 import {useEffect, useState} from "react";
+import {Accuracy, ActivityType} from "expo-location";
+
+async function requestPermissions() {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+    } else {
+        const {status: backgroundStatus} = await Location.requestBackgroundPermissionsAsync();
+        if (backgroundStatus === 'granted') {
+            await Location.startLocationUpdatesAsync('background_location_task', {
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 5000,
+                distanceInterval: 3,
+                activityType: ActivityType.Fitness
+            });
+        }
+    }
+}
 
 export const RecordScreen = () => {
-    const [location, setLocation] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null);
+    const [locations, setLocations] = useState([]);
+    const [newLocations, setNewLocations] = useState([]);
 
     useEffect(() => {
         (async () => {
-
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
-                return;
-            }
-
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
+            TaskManager.defineTask('background_location_task', ({data: {locations: locationData}, error}) => {
+                if (error) {
+                    console.log(error.message);
+                    return;
+                }
+                setNewLocations(locationData);
+                console.log(locations.length);
+            });
+            await requestPermissions();
         })();
     }, []);
 
+    useEffect(() => {
+        setLocations(locations.concat(newLocations));
+    }, [newLocations]);
 
     return (
         <View style={styles.view}>
             <MapView style={styles.map}>
-                <Marker coordinate={location !== null ? location.coords : {latitude: 0, longitude: 0}}/>
+                {locations.map((location, index) => (
+                    <Marker key={index}
+                            coordinate={{latitude: location.coords.latitude, longitude: location.coords.longitude}}/>
+                ))}
             </MapView>
         </View>
     );
