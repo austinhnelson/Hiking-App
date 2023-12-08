a684e7ad1ba114a76200e43c0d6ef94eb5d02651
Austin Nelson <nelsonhaustin@gmail.com>
2023-12-08 14:24:33 -0700

    Finished front-end for Settings

--- components/SettingsScreen.js
+++ components/SettingsScreen.js
@@ -1,20 +1,89 @@
-import {StyleSheet, Text, View} from 'react-native';
-import {Colors} from '../styles/index';
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Colors } from "../styles/index";
 
 export const SettingsScreen = () => {
-    return (
-        <View style={styles.view}>
-            <Text style={styles.text}>SETTINGS</Text>
-        </View>
-    );
-}
+  const personalDetailsValues = {
+    username: "JohnDoe",
+    password: "********",
+  };
+
+  const accountDetailsValues = {
+    numberOfFriends: 10,
+    totalActivity: 100,
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Personal Details</Text>
+        <Text style={styles.settingsField}>
+          Username: {personalDetailsValues.username}
+        </Text>
+        <Text style={styles.settingsField}>
+          Password: {personalDetailsValues.password}
+        </Text>
+      </View>
+
+      {/* Second Card */}
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Account Details</Text>
+        <Text style={styles.settingsField}>
+          Number of Friends: {accountDetailsValues.numberOfFriends}
+        </Text>
+        <Text style={styles.settingsField}>
+          Total Activity (miles): {accountDetailsValues.totalActivity}
+        </Text>
+      </View>
+      {/* Logout Button */}
+      <TouchableOpacity
+        style={styles.logoutButton}
+        onPress={() => handleLogout()}
+      >
+        <Text style={styles.logoutButtonText}>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    view: {
-        backgroundColor: Colors.background,
-    },
-    text: {
-        color: Colors.accent,
-        fontSize: 36,
-    },
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  card: {
+    backgroundColor: Colors.background,
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 16,
+    elevation: 3,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+  cardTitle: {
+    color: Colors.accent,
+    fontSize: 24,
+    marginBottom: 8,
+  },
+  settingsField: {
+    fontSize: 16,
+    backgroundColor: Colors.accent,
+    borderColor: Colors.accent,
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 10,
+    marginBottom: 8,
+    overflow: "hidden",
+  },
+  logoutButton: {
+    backgroundColor: Colors.accent,
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  logoutButtonText: {
+    color: Colors.black,
+    fontSize: 16,
+    textAlign: "center",
+  },
 });
